c285044a0ca018d4f407200514ad1c95f3bf0937
Tony Dang <tonydang0124@gmail.com>
2020-09-23 23:22:14 -1000

    updated

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,18 +1,64 @@
 import React, { Component } from 'react';
 import classes from './Button.module.css';
 import useSound from 'use-sound';
-
+import WAAClock from 'waaclock';
 class button extends Component {
-	constructor(props) {
-		super(props);
+	state = {
+		play: false,
+	};
 
-		this.state = {
-			play: false,
-		};
-        
-		this.url =
-			this.props.url;
-        this.audio = new Audio(this.url);
+	componentDidMount() {
+		this.context = new window.AudioContext();
+		this.clock = new WAAClock(this.context);
+	}
+	handlePlay = () => {
+		if (this.state.play) {
+			
+				this.currentTime = 0;
+				this.audio.load();
+				this.audio.play();
+			
+		}
+	};
+	handlePlayPress = () => {
+		if (!this.state.play) {
+			this.setState({
+				play: !this.state.play,
+			});
+			this.clock.start();
+			this.event = this.clock
+				.callbackAtTime(
+					this.handlePlay,
+					0,
+				)
+				.repeat(5.3);
+		} else {
+			this.setState({
+				play: !this.state.play,
+			});
+			this.audio.pause();
+			this.clock.stop();
+			this.event.clear();
+			this.event = null;
+		}
+	};
+
+	// togglePlay() {
+	// 	// if (wasPlaying) {
+	// 	// 	this.audio.pause();
+	// 	// 	this.currentTime = 0;
+	// 	// } else {
+	// 	// 	this.currentTime = 0;
+	// 	// 	this.audio.load();
+	// 	// 	this.audio.play();
+	// 	// }
+	// }
+
+	render() {
+	
+		this.url = this.props.url;
+		this.audio = new Audio(this.url);
+
 		this.audio.addEventListener(
 			'ended',
 			function () {
@@ -21,43 +67,23 @@ class button extends Component {
 			},
 			false
 		);
-		this.audio.addEventListener(
-			'paused',
-			function () {
-				this.currentTime = 0;
-				this.play();
-			},
-			false
-		);
-		this.togglePlay = this.togglePlay.bind(this);
-	}
-
-	togglePlay() {
-		const wasPlaying = this.state.play;
-		this.setState({
-			play: !wasPlaying,
-		});
-
-		if (wasPlaying) {
-			this.audio.pause();
-			this.currentTime = 0;
-		} else {
-			this.currentTime = 0;
-			this.audio.load();
-			this.audio.play();
-		}
-	}
-
-	render() {
+		// this.audio.addEventListener(
+		// 	'paused',
+		// 	function () {
+		// 		this.currentTime = 0;
+		// 		this.play();
+		// 	},
+		// 	false
+		// );
+		// this.togglePlay = this.togglePlay.bind(this);
 		return (
 			<div>
 				<button
 					id="audioBtn"
-					onClick={this.togglePlay}
-					className={[
-						classes.Button,
-						classes[this.state.play]
-					].join(' ')}
+					onClick={this.handlePlayPress}
+					className={[classes.Button, classes[this.state.play]].join(
+						' '
+					)}
 				>
 					1
 				</button>
